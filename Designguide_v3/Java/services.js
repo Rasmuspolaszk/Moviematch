b51bb64598ef28276c661cc38c4439f33b42ffc2
Rasmus Polaszk <70701464+Rasmuspolaszk@users.noreply.github.com>
2021-05-05 21:12:46 +0200

    Forsøgt at få Add All knap at virke

--- Designguide_v3/Java/services.js
+++ Designguide_v3/Java/services.js
@@ -10,3 +10,16 @@ button.forEach((buttons) => {
         buttons.classList.toggle('--active')
     })
 })
+
+let addAllButton = document.querySelectorAll('.addAll__btn')
+
+//buttonSelect.addEventlistener('click', () => {
+//  buttonSelect.classlist.toggle('.--active')
+//})
+console.log(addAllButton);
+
+addAllButton.forEach((buttons) => {
+    buttons.addEventListener('click', () => {
+        buttons.classList.toggle('--active')
+    })
+})
