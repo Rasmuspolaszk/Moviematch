af01012cbcdbbb57432c2aee59f27a090c0e6b8a
Rasmus Polaszk <70701464+Rasmuspolaszk@users.noreply.github.com>
2021-05-06 12:00:07 +0200

    AllApp knappen virker ikke

--- Designguide_v3/Java/genre.js
+++ Designguide_v3/Java/genre.js
@@ -11,15 +11,14 @@ button.forEach((buttons) => {
     })
 })
 
-let addAllButton = document.querySelectorAll('.addAll__btn')
 
-//buttonSelect.addEventlistener('click', () => {
-//  buttonSelect.classlist.toggle('.--active')
-//})
-console.log(addAllButton);
 
-addAllButton.forEach((buttons) => {
-    buttons.addEventListener('click', () => {
-        buttons.classList.toggle('--active')
-    })
-})
+
+
+
+
+
+/*let addAllButton = document.querySelector('.addAll__btn')
+
+function changeAllButtons() {
+    document.body.style.background = "red";*/
